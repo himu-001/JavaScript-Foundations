5d5039bd0f3cd549f52bf7b4ffe2a28b9e31fcf4
himu-001 <khimanshukumar321@gmail.com>
2026-07-11 22:57:37 +0530

    Meaningful commit message

--- 10_OOPS/important.js
+++ 10_OOPS/important.js
@@ -49,3 +49,32 @@ const user2 = new User2("abc");
 
 console.log(user2.tomPassword()); // ✅ Correct
 //  console.log(user.#password); // ❌ SyntaxError as #password is private and cannot be accessed outside the class
+
+//--------------------------------------------------------------------------//
+// using getter and setter 
+
+class User {
+    #password;
+
+    constructor(password) {
+        this.#password = password;
+    }
+
+    get password() {
+        return this.#password;
+    }
+
+    set password(value) {
+        this.#password = value;
+    }
+}
+
+const user = new User("abc");
+
+console.log(user.password); // ✅ abc
+user.password = "xyz";       // ✅ Setter runs
+
+// Because the getter and setter are inside the class, so they are allowed to access #password.
+
+// You only get a private access error when outside code tries to access the private field directly:
+// console.log(user.#password); // ❌ SyntaxError
